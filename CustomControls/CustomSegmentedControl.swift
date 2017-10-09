989dafd2f64d5ab07454c2ca8970fd73ccc2c862
Darragh Blake <darragh.blake48@gmail.com>
2017-10-09 19:43:43 +0100

    Added CustomSegmentedControl.swift

---
+++ CustomControls/CustomSegmentedControl.swift
@@ -0,0 +1,21 @@
+//
+//  CustomSegmentedControl.swift
+//  CustomControls
+//
+//  Created by Darragh on 10/9/17.
+//  Copyright © 2017 Darragh. All rights reserved.
+//
+
+import UIKit
+
+class CustomSegmentedControl: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
